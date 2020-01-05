73f86717efc30060db3689639325d64ced3caafb
Igor <igorn2304@gmail.com>
2020-01-05 22:55:42 +0300

    25 QuerySet Lookups

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,10 +1,17 @@
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
 from .models import BlogPost
 
 
+# get -> 1 object
+# filter -> [] objects
+
 def blog_post_detail_page(request, slug):
-    obj = get_object_or_404(BlogPost, slug=slug)
+    queryset = BlogPost.objects.filter(slug=slug)
+    if queryset.count() != 1:
+        raise Http404
+    obj = queryset.first()
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
