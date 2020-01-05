6d54588c8e27ee1609decff043ff215cf93d3a27
Igor <igorn2304@gmail.com>
2020-01-05 20:37:50 +0300

    14 Rendering Any Kind of template(render txt)

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 
 def home_page(request):
@@ -13,3 +14,12 @@ def about_page(request):
 
 def contact_page(request):
     return render(request, "hello_world.html", {"title": "Contact Us"})
+
+
+def example_page(request):
+    my_title = "Hello there...."
+    context = {"title": my_title}
+    template_name = "title.txt"
+    template_obj = get_template(template_name)
+    rendered_string = template_obj.render(context)
+    return render(request, "hello_world.html", {"title": rendered_string})
