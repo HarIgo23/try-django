5eed8de4cab9dcf6757446d3a1c4d76266791614
Igor <igorn2304@gmail.com>
2020-01-05 22:38:57 +0300

    23 Get Object or 404

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,14 +1,10 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import BlogPost
 
 
 def blog_post_detail_page(request, post_id):
-    try:
-        obj = BlogPost.objects.get(id=post_id)
-    except BlogPost.DoesNotExist:
-        raise Http404
+    obj = get_object_or_404(BlogPost, id=post_id)
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
