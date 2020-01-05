6c1bbbaeaa1aa57d55bb4e310d43d1193a51514d
Igor <igorn2304@gmail.com>
2020-01-05 20:14:42 +0300

    13 stay DRY with templates(base.html and extend)

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -8,7 +8,7 @@ def home_page(request):
 
 
 def about_page(request):
-    return render(request, "hello_world.html", {"title": "About Us!"})
+    return render(request, "about.html", {"title": "About Us!"})
 
 
 def contact_page(request):
