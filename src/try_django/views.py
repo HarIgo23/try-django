31eefc6fefacb56562e13dd6b5daf5f70f8e17f4
Igor <igorn2304@gmail.com>
2020-01-06 16:42:41 +0300

    34 A Django Form

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import get_template
 
+from .forms import ContactForm
 
 def home_page(request):
     my_title = "Hello there...."
@@ -13,8 +14,15 @@ def about_page(request):
 
 
 def contact_page(request):
-    print(request.POST)
-    return render(request, "form.html", {"title": "Contact Us"})
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+        form = ContactForm()
+    context = {
+        "title": "Contact Us",
+        "form": form
+    }
+    return render(request, "form.html", context)
 
 
 def example_page(request):
