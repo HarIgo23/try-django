2a6e40c05fdc275e389b08b049dba2822344b9b8
Igor <igorn2304@gmail.com>
2020-01-07 19:09:05 +0300

    46 Include with Arguments

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render
 from django.template.loader import get_template
 
 from .forms import ContactForm
+from blog.models import BlogPost
+
 
 def home_page(request):
-    my_title = "Hello there...."
-    return render(request, "home.html", {"title": my_title, "my_list": [1, 2, 3]})
+    qs = BlogPost.objects.all()[:5]
+    context = {"title": "Welcome to Try Django", "blog_list": qs}
+    return render(request, "home.html", context)
 
 
 def about_page(request):
