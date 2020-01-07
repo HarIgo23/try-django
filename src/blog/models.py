81b4eadbf488991e92ca3ad29decc99ecde2b99e
Igor <igorn2304@gmail.com>
2020-01-07 20:21:30 +0300

    48 - Publish Date, Timestamp & Updated

--- src/blog/models.py
+++ src/blog/models.py
@@ -12,14 +12,18 @@ class BlogPost(models.Model):  # user.blogpost_set -> queryset related with user
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
+    publish_data = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish_data', '-updated', '-timestamp']
 
     def get_absolute_url(self):
         return f"/blog/{self.slug}"
 
     def get_edit_url(self):
-        absolute_url = self.get_absolute_url()
-        return f"{absolute_url}/edit"
+        return f"{self.get_absolute_url()}/edit"
 
     def get_delete_url(self):
-        absolute_url = self.get_absolute_url()
-        return f"{absolute_url}/delete"
+        return f"{self.get_absolute_url()}/delete"
